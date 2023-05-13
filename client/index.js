a7c08d7a317117d0a4a303c903050b4b50e85f73
Octa717 <104462382+Octa717@users.noreply.github.com>
2023-05-13 15:00:15 +0300

    Finished project

--- client/index.js
+++ client/index.js
@@ -5,12 +5,20 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+
+  const merkleTree = new MerkleTree(niceList);
+
+  const name = process.argv[2];
+  console.log(name);
+
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof
   });
-
+  
   console.log({ gift });
 }
 
